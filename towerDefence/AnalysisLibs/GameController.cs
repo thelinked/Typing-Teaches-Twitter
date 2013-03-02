78463be845f5e237b3165e4c3a1293e1bbd8c7c8
Jack Higgins <git@jackhiggins.ie>
2013-03-02 23:15:33 +0000

    added delegates and controller class

---
+++ towerDefence/AnalysisLibs/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalysisLibs
+{
+    public class GameController
+    {
+        public delegate void SubscribeToTopic(string topic);
+
+        public delegate void TweetFrequency(int millis);
+
+        public delegate void DifficultyLevel(int difficulty);
+
+        public delegate void SendTweet(AnalysedSentence sentence);
+    }
+}
