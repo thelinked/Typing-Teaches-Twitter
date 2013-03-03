bd8f56ce54c8c6322893b956c24f14df80c903b7
Daniel Hartnett <thelinked@gmail.com>
2013-03-03 03:49:05 +0000

    Using ScoreTracker

--- TwitterTeachesTyping/MainWindow.xaml.cs
+++ TwitterTeachesTyping/MainWindow.xaml.cs
@@ -1,39 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using AnalysisLibs;
 using MahApps.Metro.Controls;
 
 namespace TwitterTeachesTyping
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : MetroWindow
     {
         private readonly GameController controller;
+
+        private readonly ScoreTracker scoreTracker;
+        private readonly Random rand;
+
         public MainWindow()
         {
             InitializeComponent();
-            //btnRaiseEvent.Click += btnRaiseEventCustomArgs_Click;
+            InitializeComponent();
             chooseTopic.TextInput += chooseTopic_MouseEnter;
             chooseTopic.KeyDown += (sender, e) => DispatchOnEnter(e, OnChooseTopic);
-
+           // scoreLabel.Content = "Score:0";
             controller = new GameController(HandleTweet);
-//            gameController.Listen(new[] { "#BeliebersHatePaparazzi" });
         }
 
         private void HandleTweet(AnalysedSentence tweet)
@@ -43,20 +32,15 @@ namespace TwitterTeachesTyping
             {
                 stackPanel.Children.Insert(0, new TextBox() { Text = tweet.Original });
             });
-
+            scoreTracker.Score = (int)(rand.Next() * 100);
+            //scoreLabel.Content = string.Format("Score:{0}", scoreTracker.Score);
         }
-
-        private void btnRaiseEventCustomArgs_Click(object sender, RoutedEventArgs e)
-        {
-            Console.WriteLine("buttonclick");   
-        }
-
-
 
         private void chooseTopic_MouseEnter(object sender, RoutedEventArgs e)
         {
             Console.WriteLine("mouseEnter");
         }
+
         private void DispatchOnEnter(KeyEventArgs e, Action onEnter )
         {
             if (e.Key == Key.Enter)
@@ -73,17 +57,5 @@ namespace TwitterTeachesTyping
                 controller.Listen(chooseTopic.Text.Split(',').Select(x => x.Trim()).ToArray());
             }
         }
-
-
-
-
-
-        private void OnTargetUpdated(object sender, DataTransferEventArgs dataTransferEventArgs)
-        {
-            Console.WriteLine("targetUpdated");
-        }
-
-
-        public string Name2 { get; set; }
     }
 }
